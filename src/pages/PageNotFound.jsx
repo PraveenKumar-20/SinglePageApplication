7fa4a38659d2b7513219dbac6bf2611481123c94
Matpathi Praveen Kumar <praveenkumar1214241@gmail.com>
2024-07-17 10:32:24 +0530

    Implemented Main Pages and Routes

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav";
+
+function PageNotFound() {
+  return (
+    <div>
+      <PageNav />
+      <h1>Page Not Found ❌</h1>
+    </div>
+  );
+}
+
+export default PageNotFound;
